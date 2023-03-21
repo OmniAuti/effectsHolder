4f08bd062d2421f09de4654bb8f2387d92fb98dd
Austin Olijar <Austin.Olijar@gmail.com>
2023-03-21 12:04:32 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,62 +1,117 @@
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const twoText = document.querySelector('.two p');
-const three = document.querySelector('.three');
-const threeText = document.querySelector('.three p');
-const four = document.querySelector('.four');
-const fourText = document.querySelector('.four p');
-const five = document.querySelector('.five');
-const fiveText = document.querySelector('.five p');
-const six = document.querySelector('.six');
-const sixText = document.querySelector('.six p');
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const twoText = document.querySelector(".two p");
+const three = document.querySelector(".three");
+const threeText = document.querySelector(".three p");
+const four = document.querySelector(".four");
+const fourText = document.querySelector(".four p");
+const five = document.querySelector(".five");
+const fiveText = document.querySelector(".five p");
+const six = document.querySelector(".six");
+const sixText = document.querySelector(".six p");
+const seven = document.querySelector(".seven");
+const sevenText = document.querySelector(".seven p");
+const ten = document.querySelector(".ten");
+const tenLineMask = document.querySelector(".line-mask");
 
-var fourArr = Array.from(fourText.childNodes)
-var fiveArr = Array.from(fiveText.childNodes);
-var sixArr = Array.from(sixText.childNodes);
+const spanOne = document.querySelector(".span-one");
+const spanTwo = document.querySelector(".span-two");
+const spanThree = document.querySelector(".span-three");
 
-   fourText.childNodes.forEach(x => {
-    x.style.transitionDelay = `${fourArr.indexOf(x) * 100}ms`
-   })
-   fiveText.childNodes.forEach(x => {
-    x.style.transitionDelay = `${fiveArr.indexOf(x) * 200}ms`
-   })
-   sixText.childNodes.forEach(x => {
-    x.style.transitionDelay = `${(sixArr.indexOf(x))* 100}ms`
-   })
+var fourArr = Array.from(fourText.children);
+var fiveArr = Array.from(fiveText.children);
+var sixArr = Array.from(sixText.children);
+var sevenArr = Array.from(sevenText.children);
 
-window.addEventListener('scroll', () => {
-    var twoHeight = window.innerHeight - two.getBoundingClientRect().top;
-    var thirdHeight = window.innerHeight - three.getBoundingClientRect().top;
-    var fourHeight = window.innerHeight - four.getBoundingClientRect().top;
-    var fiveHeight = window.innerHeight - five.getBoundingClientRect().top;
-    var sixHeight = window.innerHeight - six.getBoundingClientRect().top;
-   if (twoHeight > window.innerHeight / 3) {
-    twoText.classList.add('active-two')
-   }
-   if (thirdHeight > window.innerHeight / 3) {
-    threeText.classList.add('active-three')
-   }
+fourArr.forEach((x) => {
+  x.style.transitionDelay = `${fourArr.indexOf(x) * 100}ms`;
+});
+fiveArr.forEach((x) => {
+  x.style.transitionDelay = `${fiveArr.indexOf(x) * 200}ms`;
+});
+sixArr.forEach((x) => {
+  x.style.transitionDelay = `${sixArr.indexOf(x) * 100}ms`;
+});
+sevenArr.forEach((x) => {
+  x.style.transitionDelay = `${sevenArr.indexOf(x) * 100}ms`;
+});
 
+window.addEventListener("scroll", () => {
+  var twoHeight = window.innerHeight - two.getBoundingClientRect().top;
+  var thirdHeight = window.innerHeight - three.getBoundingClientRect().top;
+  var fourHeight = window.innerHeight - four.getBoundingClientRect().top;
+  var fiveHeight = window.innerHeight - five.getBoundingClientRect().top;
+  var sixHeight = window.innerHeight - six.getBoundingClientRect().top;
+  var sevenHeight = window.innerHeight - seven.getBoundingClientRect().top;
 
-   if (fourHeight > window.innerHeight / 3) {
-    fourText.childNodes.forEach(x => {
-        x.style.transform = `translateX(0)`
-        x.style.opacity = `1`
-       })
-   }
+  if (twoHeight > window.innerHeight / 3) {
+    twoText.classList.add("active-two");
+  }
 
-   if (fiveHeight > window.innerHeight / 3) {
-    fiveText.childNodes.forEach(x => {
-        x.style.fontSize = '100px'
-    })
-   }
-   if (sixHeight > window.innerHeight / 3) {
-    sixText.childNodes.forEach(x => {
-        x.style.textShadow = '-2.5px 2.5px 7.5px #000, 2.5px -2.5px 10px #000'
-    })
-   }
+  if (thirdHeight > window.innerHeight / 3) {
+    threeText.classList.add("active-three");
+  }
 
+  if (fourHeight > window.innerHeight / 3) {
+    fourArr.forEach((x) => {
+      x.style.transform = `translateX(0)`;
+      x.style.opacity = `1`;
+    });
+  }
 
-})
+  if (fiveHeight > window.innerHeight / 3) {
+    fiveArr.forEach((x) => {
+      x.style.fontSize = "100px";
+    });
+  }
 
+  if (sixHeight > window.innerHeight / 3) {
+    sixArr.forEach((x) => {
+      x.style.textShadow = "-2.5px 2.5px 0px #000, -2.5px 2.5px 0px #000";
+    });
+  }
 
+  if (sevenHeight > window.innerHeight / 3) {
+    sevenArr.forEach((x) => {
+      x.style.transform = "translateY(0)";
+    });
+  }
+
+  if (
+    spanOne.getBoundingClientRect().top < window.innerHeight / 3 &&
+    spanOne.getBoundingClientRect().top > 250
+  ) {
+    spanOne.classList.add("span-active");
+    spanThree.classList.remove("span-active");
+    spanTwo.classList.remove("span-active");
+  } else if (
+    spanTwo.getBoundingClientRect().top < window.innerHeight / 3 &&
+    spanTwo.getBoundingClientRect().top > 250
+  ) {
+    spanOne.classList.remove("span-active");
+    spanTwo.classList.add("span-active");
+    spanThree.classList.remove("span-active");
+  } else if (
+    spanThree.getBoundingClientRect().top < window.innerHeight / 3 &&
+    spanThree.getBoundingClientRect().top > 250
+  ) {
+    spanTwo.classList.remove("span-active");
+    spanThree.classList.add("span-active");
+  }
+
+  var windowHalf = window.innerHeight / 2;
+
+  // console.log(ten.clientHeight, window.innerHeight);
+  var lineMaskWidth =
+    ((ten.getBoundingClientRect().top - ten.clientHeight) /
+      (windowHalf - ten.clientHeight)) *
+    100;
+  console.log(lineMaskWidth);
+  if (lineMaskWidth <= 0) {
+    tenLineMask.style.width = 0;
+  } else if (lineMaskWidth >= 110) {
+    lineMaskWidth = 110;
+  } else {
+    tenLineMask.style.width = `${lineMaskWidth}%`;
+  }
+});
